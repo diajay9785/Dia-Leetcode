fc37ea5130f2d811f3ed8f2da1705df5df2fe8d1
Divya Jayaprakash <diajayaprakash9785@gmail.com>
2026-08-28 14:42:48 +0530

    Time: 127 ms (94.20%) | Memory: 108 MB (80.91%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,31 +1,49 @@
 class TimeMap {
-    HashMap<String, TreeMap<Integer, String>> map;
+    HashMap<String, ArrayList<Pair>> map;
     public TimeMap() {
         map = new HashMap<>();
     }
-    
     public void set(String key, String value, int timestamp) {
-       if (!map.containsKey(key)) {
-            map.put(key, new TreeMap<>());
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
         }
-        map.get(key).put(timestamp, value);
+        map.get(key).add(new Pair(timestamp, value));
     }
-    
     public String get(String key, int timestamp) {
+
         if (!map.containsKey(key)) {
             return "";
         }
-        Map.Entry<Integer, String> entry =map.get(key).floorEntry(timestamp);
-        if (entry == null) {
-            return "";
+        ArrayList<Pair> list = map.get(key);
+
+        int left = 0;
+        int right = list.size() - 1;
+
+        String answer = "";
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (list.get(mid).timestamp <= timestamp) {
+                answer = list.get(mid).value;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        return entry.getValue();
+
+        return answer;
+    }
+    class Pair {
+
+        int timestamp;
+        String value;
+
+        Pair(int timestamp, String value) {
+            this.timestamp = timestamp;
+            this.value = value;
+        }
     }
 }
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap obj = new TimeMap();
- * obj.set(key,value,timestamp);
- * String param_2 = obj.get(key,timestamp);
- */
