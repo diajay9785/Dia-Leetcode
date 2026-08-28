3699157993f606328e3243487a12c522c7b61afa
Divya Jayaprakash <diajayaprakash9785@gmail.com>
2026-08-28 21:29:59 +0530

    Time: 7 ms (20.08%) | Memory: 44.1 MB (36.98%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -1,24 +1,32 @@
 class Solution {
     public String capitalizeTitle(String title) {
+
         title = title.toLowerCase();
-        String words[] = title.split("\\s+");
-        String ans = "";
+
+        String[] words = title.split("\\s+");
+
+        StringBuilder ans = new StringBuilder();
+
         for (int i = 0; i < words.length; i++) {
+
             String word = words[i];
+
             if (word.length() <= 2) {
-                ans += word;
-            }
+                ans.append(word);
+            } 
             else {
-                ans += Character.toUpperCase(word.charAt(0));
+                ans.append(Character.toUpperCase(word.charAt(0)));
 
                 for (int j = 1; j < word.length(); j++) {
-                    ans += word.charAt(j);
+                    ans.append(word.charAt(j));
                 }
             }
+
             if (i < words.length - 1) {
-                ans += " ";
+                ans.append(" ");
             }
         }
-        return ans;
+
+        return ans.toString();
     }
 }
