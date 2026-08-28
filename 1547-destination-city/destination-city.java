2a830b746b5668a8e36e6cc434a420e34db145ba
Divya Jayaprakash <diajayaprakash9785@gmail.com>
2026-08-28 23:01:21 +0530

    Time: 2 ms (99.85%) | Memory: 44.9 MB (62.57%) - LeetSync

---
+++ 1547-destination-city/destination-city.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashSet<String> set = new HashSet<>();
+        for (List<String> path : paths) {
+            set.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            String destination = path.get(1);
+            if (!set.contains(destination)) {
+                return destination;
+            }
+        }
+        return "";
+    }
+}
