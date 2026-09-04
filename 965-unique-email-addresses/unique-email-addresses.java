5f5c18287a9f2c38016081fce542d42f9e727cc8
Divya Jayaprakash <diajayaprakash9785@gmail.com>
2026-09-04 14:49:54 +0530

    Time: 7 ms (95.36%) | Memory: 46.3 MB (93.30%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.java
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -1,33 +1,32 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
+
         HashSet<String> set = new HashSet<>();
+
         for (int i = 0; i < emails.length; i++) {
-            String word = emails[i];
-            String ans = "";
-            boolean stop = false;
-            boolean domain = false;
-            for (int j = 0; j < word.length(); j++) {
-                char ch = word.charAt(j);
-                if (ch == '@') {
-                    domain = true;
-                    stop = false;
-                    ans += ch;
-                    continue;
-                }
-                if (stop == true) {
-                    continue;
-                }
-                if (ch == '+' && domain == false) {
-                    stop = true;
-                    continue;
-                }
-                if (ch == '.' && domain == false) {
-                    continue;
-                }
-                ans += ch;
+
+            String email = emails[i];
+
+            int at = email.indexOf('@');
+            int plus = email.indexOf('+');
+
+            String local;
+
+            if (plus != -1 && plus < at) {
+                local = email.substring(0, plus);
+            } else {
+                local = email.substring(0, at);
             }
-            set.add(ans);
+
+            local = local.replace(".", "");
+
+            String domain = email.substring(at);
+
+            String finalEmail = local + domain;
+
+            set.add(finalEmail);
         }
+
         return set.size();
     }
 }
